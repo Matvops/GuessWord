b8d0d10afe7c38abf552c065f7c31706f03f595a
Matheus <matheusmartinscia@gmail.com>
2026-03-01 21:26:13 -0300

    Feat: Altera amarelo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import './styles/theme.css';
 import './styles/global.css';
+import { Container } from './components/Container';
+import { Navbar } from './components/Navbar';
+import { Lines } from './components/Lines';
 
 
 function App() {
 
   return (
     <>
+      <Container>
+        <Navbar />
+      </Container>
+
+      <Container>
+        <Lines />
+      </Container>
+
     </>
   );
 }
